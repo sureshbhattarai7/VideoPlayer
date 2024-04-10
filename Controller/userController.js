e158a917d88bdb6608ecf48a3395b15ef9d41438
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-04-10 21:57:51 +0545

    Implement Create User functionality

--- Controller/userController.js
+++ Controller/userController.js
@@ -34,3 +34,28 @@ exports.getUser = async (req, res, next) => {
         })
     }
 }
+
+exports.signup = async (req, res, next) => {
+    const user = await User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    try {
+        res.status(200).json({
+            status: 'success',
+            data: {
+                user
+            }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        })
+    };
+};
