45f0f360576702909552583ac9c3fa4b105b29a3
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-04-11 17:39:40 +0545

    Implement getUsers and getUser route

--- Router/userRoute.js
+++ Router/userRoute.js
@@ -6,6 +6,10 @@ const router = express.Router();
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 
+router.route('/')
+    .get(userController.getUsers);
 
+router.route('/:id')
+    .get(userController.getUser);
 
 module.exports = router;
