cedd61e33e2c272c20a567fb738ee2d0612b52ac
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-04-12 17:59:32 +0545

    Implement dbConnection with Sequelize

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -8,14 +8,30 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
     dialect: 'mssql',
     dialectOptions: {
         options: {
+            database: process.env.DB_DATABASE,
             encrypt: false
         }
     }
 });
 
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection to the database has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
 
-// new sqlInstance.ConnectionPool(dotenv)
-
-
+async function syncModels() {
+    try {
+        await sequelize.sync();
+        console.log("Models have been synchronized with the database.");
+    } catch (error) {
+        console.error("Unable to sync models with the database:", error);
+    }
+}
+syncModels();
+testConnection();
 
 module.exports = sequelize;
