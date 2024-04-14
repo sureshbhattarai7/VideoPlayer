2e30463c3c0e3d9c09e75fafcc5807a6187b9bd6
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-04-14 16:29:02 +0545

    Implement Database Connection!

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -1,37 +1,34 @@
 const { Sequelize } = require('sequelize');
+const sql = require('mssql');
 const dotenv = require('dotenv');
 dotenv.config({ path: "./config.env" });
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.PORT),
-    dialect: 'mssql',
-    dialectOptions: {
-        options: {
-            database: process.env.DB_DATABASE,
-            encrypt: false
-        }
-    }
-});
 
-async function testConnection() {
+const sqlConfig = {
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    server: 'localhost',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+}
+
+const testConnection = async () => {
     try {
-        await sequelize.authenticate();
-        console.log("Connection to the database has been established successfully.");
+        await sql.connect(sqlConfig);
+        console.log("Connection to the database has been established successfully!");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
     }
-}
-
-async function syncModels() {
-    try {
-        await sequelize.sync();
-        console.log("Models have been synchronized with the database.");
-    } catch (error) {
-        console.error("Unable to sync models with the database:", error);
-    }
-}
-syncModels();
+};
 testConnection();
 
-module.exports = sequelize;
+
+module.exports = sqlConfig;
